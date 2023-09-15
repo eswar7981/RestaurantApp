b6fc936e3b75df80221aba01d65155db60559241
eswar7981 <126505498+eswar7981@users.noreply.github.com>
2023-09-15 11:02:31 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-
+import MealsSummary from './Components/Meals/MealsSummary';
 import { Fragment, useEffect, useState } from 'react';
 import Header from './Components/Layout/Header';
 function App() {
@@ -9,10 +9,14 @@ function App() {
 
   return (
     <Fragment>
-     <Header/>
+      <Header/>
+
+      <MealsSummary/>
+   
     </Fragment>
 
   );
 }
 
 export default App;
+
