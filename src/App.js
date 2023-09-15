f979d88ed55e825d1459fd7408922ffec47d89cb
eswar7981 <126505498+eswar7981@users.noreply.github.com>
2023-09-15 20:46:13 +0530

    Created Portal

--- src/App.js
+++ src/App.js
@@ -5,17 +5,26 @@ import { Fragment, useEffect, useState } from 'react';
 import Header from './Components/Layout/Header';
 import mealimg from './images/meals.jpg'
 import AvailableMeals from './Components/Meals/AvailableMeals';
+
+import Portal from './Components/Portal/Portal';
+
 function App() {
 
  
 
   return (
-    <Fragment>
-      <Header/>
-      <MealsSummary/>
-      <AvailableMeals/>
-       
    
+  
+    <Fragment>
+      
+      <Header/>
+      
+      <MealsSummary/>
+      
+      <AvailableMeals/>
+      <Portal/>
+  
+      
    
     </Fragment>
 
