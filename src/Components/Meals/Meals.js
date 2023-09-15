1c97cc8e0d23fdce88d342a52335107394ef5f38
eswar7981 <126505498+eswar7981@users.noreply.github.com>
2023-09-15 15:16:06 +0530

    Added Add button Meals.js

--- src/Components/Meals/Meals.js
+++ src/Components/Meals/Meals.js
@@ -1,17 +1,27 @@
 import React from "react";
 import "./Meals.css";
+import AddButton from "./AddButton";
 const Meals = (props) => {
   const menu = props.menu;
   return (
     <div className="Meals">
       {menu.map((item) => (
+        <>
+        <div className="display">
         <div className="item">
 
           <h1 style={{fontSize:'20px'}}>{item.Name}</h1>
           <label style={{fontFamily:'Brush Script MT'}}>{item.Description}</label>
           <h3 style={{color:' #8a2b06',border:'solid',borderColor:'black',marginRight:'520px'}}>{item.price}</h3>
-          <br></br>
+         
+         
         </div>
+        <div className="ad">
+        <AddButton></AddButton>
+        </div>
+        
+        </div>
+        </>
       ))}
     </div>
   );
