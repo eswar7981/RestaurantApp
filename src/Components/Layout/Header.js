96559353d0ca89e78bcbc6af6844e50b312e0d02
eswar7981 <126505498+eswar7981@users.noreply.github.com>
2023-09-17 16:54:46 +0530

    Update Header.js

--- src/Components/Layout/Header.js
+++ src/Components/Layout/Header.js
@@ -1,21 +1,22 @@
 import React, { Fragment, cloneElement } from "react";
-import mealimg from '../../images/meals.jpg'
+import mealimg from "../../images/meals.jpg";
 import classes from "./Header.css";
 import HeaderCartButton from "./HeaderCartButton";
 
-export default function Header() {
+export default function Header(props) {
   return (
     <Fragment>
-    <header className="header">
+      <header className="header">
         <h1>ReactMeals</h1>
-        <HeaderCartButton/>
+        <HeaderCartButton onClick={props.openCart1}/>
         <h1></h1>
+      </header>
+      <div className="main-image">
        
-        
-    </header>
-    <div className='main-image'>
-        <img src={mealimg}/>
-    </div>
+          {" "}
+          <img src={mealimg} />
+
+      </div>
     </Fragment>
   );
 }
