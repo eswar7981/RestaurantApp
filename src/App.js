3689c9ba6ae7d64453be86f73e8c371742f472be
eswar7981 <126505498+eswar7981@users.noreply.github.com>
2023-09-20 09:52:02 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,45 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
-import MealsSummary from './Components/Meals/MealsSummary';
-import { Fragment, useEffect, useState } from 'react';
-import Header from './Components/Layout/Header';
-import mealimg from './images/meals.jpg'
-import AvailableMeals from './Components/Meals/AvailableMeals';
-import Portal from './Components/Portal/Portal';
+import logo from "./logo.svg";
+import "./App.css";
+import MealsSummary from "./Components/Meals/MealsSummary";
+import { Fragment, useEffect, useState } from "react";
+import Header from "./Components/Layout/Header";
+import mealimg from "./images/meals.jpg";
+import AvailableMeals from "./Components/Meals/AvailableMeals";
+import Portal from "./Components/Portal/Portal";
+
+import Cartprovider from "./Components/Store/Cartprovider";
+import cartContext from "./Components/Store/CartContext";
+import CartContext from "./Components/Store/CartContext";
 
 function App() {
- 
-  const [cartButton,setCartButton]=useState(false)
- 
+  const [cartButton, setCartButton] = useState(false);
 
-  const openCart=()=>{
+  const openCart = () => {
+    setCartButton(true);
+  };
 
-    setCartButton(true)
-  }
-
- 
-  const closeCart=()=>{
-    
-    setCartButton(false)
-  }
+  const closeCart = () => {
+    setCartButton(false);
+  };
 
   return (
-   
-  
-    <Fragment>
-     
-      <Header openCart1={openCart}/>
-      
-      <MealsSummary/>
-     {cartButton && <Portal  onClose={closeCart}/>}
-      <AvailableMeals/>
-      
-  
-      
-   
-    </Fragment>
+    <>
+      <Cartprovider>
+        <Header openCart1={openCart} />
 
+        <MealsSummary />
+        {cartButton && <Portal onClose={closeCart} />}
+        <AvailableMeals />
+      </Cartprovider>
+    </>
   );
 }
 
 export default App;
+
