8a32146119d8652dc1c1b8174557aa1798b6a821
eswar7981 <126505498+eswar7981@users.noreply.github.com>
2023-09-20 10:02:06 +0530

    Update AddButton.js

--- src/Components/Meals/AddButton.js
+++ src/Components/Meals/AddButton.js
@@ -1,19 +1,41 @@
+import React, { useContext, useState } from "react";
+import "./AddButton.css";
+import CartContext from "../Store/CartContext";
+const AddButton = (props) => {
+  const [quantity, setQuantity] = useState(0);
+  const cxt = useContext(CartContext);
 
-import React from 'react'
-import './AddButton.css'
-const AddButton = () => {
+  const cartAddHandler = (e) => {
+    e.preventDefault();
+    if(parseInt(quantity)>0){
+      
+    cxt.addItem({ ...props.item, quantity }, quantity);
+     setQuantity(0)
+  }};
+
+  const quantityHandler = (e) => {
+    setQuantity(e.target.value);
+  };
+
   return (
     <>
-    <form >
-        <div className='first-row'>
-        <label htmlfor='add'>Amount</label>&nbsp;&nbsp;&nbsp;
-        <input type="number" min={0}   id='add'></input><br></br>
-        <button >+ Add</button>
+      <form>
+        <div className="first-row">
+          <label htmlFor="add">Amount</label>&nbsp;&nbsp;&nbsp;
+          <input
+            value={quantity}
+            type="number"
+            min={0}
+            id="add"
+            onChange={quantityHandler}
+          ></input>
+          <br></br>
+          <button onClick={cartAddHandler}>+ Add</button>
         </div>
-       
-    </form>
+      </form>
     </>
-  )
-}
+  );
+};
 
-export default AddButton
+export default AddButton;
+
