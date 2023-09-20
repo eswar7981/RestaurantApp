36885da7c01aa7f7ead7d9d6906af98ded7c4751
eswar7981 <126505498+eswar7981@users.noreply.github.com>
2023-09-20 10:01:40 +0530

    Update AvailableMeals.js

--- src/Components/Meals/AvailableMeals.js
+++ src/Components/Meals/AvailableMeals.js
@@ -1,29 +1,43 @@
 import React from "react";
 import Meals from "./Meals";
 import { useState } from "react";
+
 const AvailableMeals = () => {
-  const [Menu,setMenu] = useState([
-    { Name: "Sushi", Description: "Finest fish and veggies", price: "$22.99" },
-    { Name: "Schnitzel", Description: "A german speciality!", price: "$16.50" },
+  const [Menu, setMenu] = useState([
     {
+      id: "m1",
+      Name: "Sushi",
+      Description: "Finest fish and veggies",
+      price: 22.99,
+    },
+    {
+      id: "m2",
+      Name: "Schnitzel",
+      Description: "A german speciality!",
+      price: 16.5,
+    },
+    {
+      id: "m3",
       Name: "Barbecue Burger",
       Description: "American, raw, meaty",
-      price: "$12.99",
+      price: 12.99,
     },
     {
+      id: "m4",
       Name: "Green Bowl",
       Description: "Healthy... and green",
-      price: "$18.00",
+      price: 18.0,
     },
   ]);
-  console.log(Menu);
+
   return (
     <>
-    <div>
-      <Meals menu={Menu}></Meals>
-    </div>
+      <div>
+        <Meals menu={Menu}></Meals>
+      </div>
     </>
   );
 };
 
 export default AvailableMeals;
+
