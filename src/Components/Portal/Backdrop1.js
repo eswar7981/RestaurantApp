dcffbe0b7e40f928993363d7ecaf4f78dad4941f
eswar7981 <126505498+eswar7981@users.noreply.github.com>
2023-09-20 09:54:16 +0530

    Update Backdrop1.js

--- src/Components/Portal/Backdrop1.js
+++ src/Components/Portal/Backdrop1.js
@@ -1,27 +1,40 @@
-import React from 'react'
-
+import React, { useContext } from "react";
+import CartContext from "../Store/CartContext";
+import ItemsInCart from "./ItemsInCart";
 const Backdrop1 = (props) => {
+  const ctx = useContext(CartContext);
+  
+  const orderHandler=()=>{
+    if(ctx.totalAmount>0) 
+    {console.log("ordering......")}
+  }
   return (
     <>
-    <div className="backdrop">
-    <div>
-      <div className="modal">
-        <div className="item">
-          <label>Sushi</label>
-        </div>
-        <div className="Amount">
-          <label>Total Amount</label>
-          <label>35.65</label>
-        </div>
-        <div className="buttons">
-          <button onClick={()=>props.onClick1()} >close</button>
-          <button >order</button>
+      <div className="backdrop">
+        <div>
+          <div className="modal">
+            <div className="item">
+              <div className="head">
+                <label>Item</label>
+                <h4>Quantity</h4>
+              </div>
+              <div className="disp">
+                <ItemsInCart />
+              </div>
+            </div>
+            <div className="Amount">
+              <label>Total Amount</label>
+              <label>{`$${ctx.totalAmount}`}</label>
+            </div>
+            <div className="buttons">
+              <button onClick={() => props.onClick1()}>close</button>
+              <button onClick={orderHandler}>order</button>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-  </div>
-  </>
-  )
-}
+    </>
+  );
+};
 
 export default Backdrop1;
